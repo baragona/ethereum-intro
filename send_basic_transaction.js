73ba8cab9008a810bed641b16a9977f99472a212
Kevin Baragona <kmbaragona@gmail.com>
2019-01-09 21:41:25 -0800

    refactor to run under nodejs

--- send_basic_transaction.js
+++ send_basic_transaction.js
@@ -1,6 +1,33 @@
-// send from one account to another
+// send from one account 0 to account 1 to another
 
-var sender = eth.accounts[0];
-var receiver = eth.accounts[1];
-var amount = web3.toWei(0.001, "ether");
-eth.sendTransaction({from:sender, to:receiver, value: amount});
+var { web3, Web3 } = require('./local_geth_connect');
+var eth = web3.eth;
+
+
+
+(async function(){
+    try{
+        console.log('getting accounts..');
+        var accounts = await eth.getAccounts();
+        var sender = accounts[0];
+        var receiver = accounts[1];
+
+        var amount = web3.utils.toWei("0.001", "ether");
+        var tx = await eth.sendTransaction({from:sender, to:receiver, value: amount});
+        console.log('sent transaction',tx);
+        process.exit();
+
+    }catch(e){
+        console.log('caught error', e);
+    }
+
+})();
+
+
+
+
+
+
+
+
+
