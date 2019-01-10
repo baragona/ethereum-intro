617d45f2c850453b05768f352bf6fe0cb5277408
Kevin Baragona <kmbaragona@gmail.com>
2019-01-09 21:20:29 -0800

    refactor get_balance.js to run under Node.js instead of geth js

--- get_balance.js
+++ get_balance.js
@@ -1,1 +1,28 @@
-console.log(web3.fromWei(eth.getBalance(eth.accounts[0]), "ether"))
+const homedir = require('os').homedir();
+
+var Web3 = require('web3');
+var net = require('net');
+
+var web3 = new Web3(homedir+'/Library/Ethereum/geth.ipc', net);
+var eth = web3.eth;
+
+(async function(){
+    try{
+        console.log('getting accounts..');
+        var accounts = await eth.getAccounts();
+        for (var acct of accounts){
+            var balance = await eth.getBalance(acct);
+            console.log(`balance of ${acct}:  ${web3.utils.fromWei(balance, "ether")} ` );
+        }
+
+        process.exit();
+
+    }catch(e){
+        console.log('caught error', e);
+    }
+
+})();
+
+
+
+
