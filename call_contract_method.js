75531a0d35fddf2579050a94887769da42a8ef20
Kevin Baragona <kmbaragona@gmail.com>
2019-01-11 20:11:22 -0800

    update call_contract_method.js to include abi for hash table

--- call_contract_method.js
+++ call_contract_method.js
@@ -13,11 +13,11 @@ var fs = require('fs');
         var sender = accounts[0];
 
         //var filename = process.argv[2];
-        var abi_filename = process.argv[2];
-        var contract_address = process.argv[3];
-        var abi_json  = JSON.parse(fs.readFileSync(abi_filename,'utf-8'));
+        //var abi_filename = process.argv[2];
+        var contract_address = process.argv[2];
+        //var abi_json  = JSON.parse(fs.readFileSync(abi_filename,'utf-8'));
         //console.log('got ABI json:', abi_json);
-
+        var abi_json = [{"constant":false,"inputs":[{"name":"key","type":"uint256"},{"name":"value","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"key","type":"uint256"}],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"hashTableValues","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
         var contractObj = new eth.Contract(abi_json, contract_address, {});
 
         console.log('loaded contract obj', contractObj);
